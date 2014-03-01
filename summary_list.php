1ac75df16c85aa7bc6247871ef82ebc3c95c93c0
Paul Patchell <patchell.paul@gmail.com>
2014-03-01 15:33:43 +0000

    Change comments

--- summary_list.php
+++ summary_list.php
@@ -9,10 +9,8 @@
 
 <?php if (!$summary_list) { ?>
 <div class="alert alert-block">
-<h4 class="alert-heading">No summaries created</h4>
-<p>To add an event based notification:</p>
-<p>1) Select the feed you wish to be notified about from the drop down menu</p>
-<p>2) Select whether you want to be notifed if the feed goes above, below or equals the value specified, or is inactive.</p>
-<p>3) Enter a value</p>
+<h4 class="alert-heading">No Summary tables detected</h4>
+<p> To add the summary tables:</p>
+<p>1) Go to Admin and Update & Check</p>
 </div>
 <?php }
