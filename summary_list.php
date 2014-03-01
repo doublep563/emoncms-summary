0d8a8f45541fd01864118629e5821b42e32c68ad
Paul Patchell <patchell.paul@gmail.com>
2014-03-01 15:42:35 +0000

    Get the feeds

--- summary_list.php
+++ summary_list.php
@@ -13,4 +13,11 @@
 <p> To add the summary tables:</p>
 <p>1) Go to Admin and Update & Check</p>
 </div>
-<?php }
+<?php } else { ?>
+<div class="alert alert-block">
+<h4 class="alert-heading">Get a list of feeds</h4>
+<p> Select * from feeds:</p>
+<h4 class="alert-heading">Get a list of summary feeds</h4>
+<p> Select * from feed_summary_list:</p>
+</div>
+<?php } ?>
